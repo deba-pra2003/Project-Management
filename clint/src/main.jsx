138269891ca6a7cf48b8c073940f64b4a37c8bb7
Debasish Pradhan <pradhandebasish2003@gmail.com>
2026-05-12 11:03:48 +0530

    first commit

---
+++ clint/src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { store } from './app/store.js'
+import { Provider } from 'react-redux'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// Get Clerk key from .env
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+ 
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing publishable key')
+}
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ClerkProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+)
